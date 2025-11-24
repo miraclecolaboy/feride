75bd0748a95427c70c3a94cba089fd652c0936ba
miraclecolaboy <miraclecola4@gmail.com>
2025-11-24 17:17:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+// Kayıt ekleme fonksiyonu
+function addCountdown() {
+    const eventName = document.getElementById("name").value;
+    const targetDate = document.getElementById("date-input").value;
+    
+    if (!(eventName && targetDate)) {
+        alert("Etkinlik adı veya tarihini girin.");
+        return;
+    }
+
+    const eventDate = new Date(targetDate);
+    const currentDate = new Date();
+    const timeDiff = eventDate - currentDate;
+    const dayDiff = Math.floor(timeDiff / (3600 * 1000 * 24));
+    
+    displayRecord(eventName, dayDiff);
+
+    const saveRecord = { eventName: eventName, dayDiff: dayDiff }; 
+    const records = JSON.parse(localStorage.getItem('records')) || [];
+    records.push(saveRecord);
+    localStorage.setItem('records', JSON.stringify(records));  
+}
+
+// Sayfa yüklendiğinde verileri yükle
+function loadrecords() {        
+    const records = JSON.parse(localStorage.getItem('records')) || [];
+    records.forEach(record => {
+        displayRecord(record.eventName, record.dayDiff);
+    });
+}
+window.onload = loadrecords;
+
+// Etkinlik ve gün farkını ekranda gösterme fonksiyonu
+function displayRecord(eventName, dayDiff) {
+    let countdownText;
+    let countupText;
+
+    // Gün farkını kontrol et
+    if (dayDiff > 0) {
+        countdownText = `${eventName} için ${dayDiff} gün kaldı`;
+    } else if (dayDiff < 0) {
+        countupText = `${eventName} ${Math.abs(dayDiff)} gün önceydi`;
+    } else {
+        countdownText = `${eventName} bugünde!`;
+    }
+
+    const countdownDiv = document.createElement("div");
+    countdownDiv.classList.add("countdown-item");
+
+    const countdownContent = document.createElement("span");
+    countdownContent.classList.add("countdown-box");
+    countdownContent.textContent = countdownText;
+
+    const countupContent = document.createElement("span");
+    countupContent.classList.add("countup-box");
+    countupContent.textContent = countupText;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Sil";
+    deleteBtn.onclick = function() {
+        removeRecord(eventName, dayDiff, countdownDiv);
+    }
+
+    countdownDiv.appendChild(countdownContent);
+    countdownDiv.appendChild(countupContent);
+    countdownDiv.appendChild(deleteBtn);
+
+    const countdownContainer = document.getElementById("countdowns");
+    countdownContainer.appendChild(countdownDiv);
+}
+
+// Kayıt silme fonksiyonu
+function removeRecord(eventName, dayDiff, countdownDiv) {
+    const records = JSON.parse(localStorage.getItem('records')) || [];
+    const updatedRecords = records.filter(record => !(record.eventName === eventName && record.dayDiff === dayDiff));
+    localStorage.setItem('records', JSON.stringify(updatedRecords));
+    countdownDiv.remove();
+}
